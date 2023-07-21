c9005d62fb7c47fafed8eecf2de5aa1c093ac41a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-21 08:51:02 +0300

    modified App.jsx and index.css in src

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="fle">Regex Golf</div>
     </>
   );
 }
