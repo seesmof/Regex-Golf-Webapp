9a90e873cae40ab732e74ca135edc0a49390b3df
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-23 14:29:04 +0300

    modified Home.jsx in pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,15 @@
-homereact;
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <div className="h-screen overflow-scroll bg-background">
+        <div className="flex flex-col container mx-auto max-w-4xl p-4">
+          <h1 className="font-medium text-xl">Home</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
