e1c9dcbd1db7e70c735bb8e01ca23d1619afc428
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-23 14:25:23 +0300

    modified Home.jsx and NotFound.jsx in pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,25 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <div className="flex flex-col space-y-4 p-4">
-        <h1>Home</h1>
+      <div className="h-screen overflow-scroll bg-orange-400 font-medium text-white">
+        <div className="flex flex-col max-w-4xl mx-auto p-4 h-full">
+          <div className="flex flex-1 flex-col items-center space-y-4">
+            <h1 className="text-3xl font-bold">Home</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Asperiores, eos.
+            </p>
+          </div>
+
+          <div className="flex justify-center">
+            <Link
+              to="/about"
+              className="px-6 py-2 rounded-xl bg-orange-600 text-center"
+            >
+              Go!
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,1 +1,22 @@
-notfoundjsx;
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <>
+      <div className="h-screen flex flex-col max-w-xl mx-auto w-full justify-center text-center p-4">
+        <h1 className="font-bold text-4xl mb-3">Ooops...</h1>
+        <p className="text-slate-700 mb-12">
+          The page you are looking for does not exist
+        </p>
+        <Link
+          to="/"
+          className="w-full py-3 bg-black hover:bg-stone-800 text-white font-medium text-center"
+        >
+          Take Me Back
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
